cb1b1cab2424d4a09e2aeb4c28238789bf38a423
TAHIRY-TS <tahiryandriatefy82@gmail.com>
2025-05-24 22:29:23 +0300

    Update compte_manager.py

--- compte_manager.py
+++ compte_manager.py
@@ -296,14 +296,14 @@ def menu():
             clear()
             titre_section("OUVERTURE DU SCRIPT DE RECONNECTION")
             for i in range(3, 0, -1):
-                print(f"\033[1;36mRetour au script principal dans {i}...\033[0m", end='\r')
+                print(f"\033[1;36mOuverture de script de reconnection dans {i} secondes...\033[0m", end='\r')
                 time.sleep(3)
                 os.execvp("python", ["python", os.path.join(PROJECT_DIR, "ts_login.py")])
         elif choix == '0':
             clear()
             titre_section("RETOUR AU MENU PRINCIPAL")
             for i in range(3, 0, -1):
-                print(f"\033[1;36mRetour à l'accueil dans {i}...\033[0m", end='\r')
+                print(f"\033[1;36mRetour à l'accueil dans {i} secondes ...\033[0m", end='\r')
                 time.sleep(3)
                 os.execvp("bash", ["bash", os.path.join(PROJECT_DIR, "start.sh")]) 
         else:
