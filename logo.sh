78593c40d2bbc22df7f54e124c876e49e39647ba
TAHIRY-TS <tahiryandriatefy82@gmail.com>
2025-05-28 04:16:02 +0300

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -9,9 +9,14 @@ logo=(
 "   ╚═╝   ╚══════╝"
 )
 
+colors=(31 33 32 36 34 35) # Red, Yellow, Green, Cyan, Blue, Magenta
+
 term_width=$(tput cols)
+i=0
 
 for line in "${logo[@]}"; do
     padding=$(( (term_width - ${#line}) / 2 ))
-    printf "%*s\e[1;36m%s\e[0m\n" "$padding" "" "$line"
+    color=${colors[$((i % ${#colors[@]}))]}
+    printf "%*s\e[1;${color}m%s\e[0m\n" "$padding" "" "$line"
+    ((i++))
 done
